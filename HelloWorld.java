a0b72e4efce5ec9836bc3494b6cd173cdf451def
sara russert <sarabeth.russert@gmail.com>
2024-01-03 16:04:50 -0800

    first assignment complete

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+//author: Sara Russert
+//date: 3 January 2024
+//file: HelloWorld.java
+
+/*
+   Lab0
+   Introduction to Java and the jGrasp IDE
+   program displays the text "Hello World"
+*/
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      //display the text "Hello World" in the output console
+      System.out.println("Hello World");
+   }
+}
+   
